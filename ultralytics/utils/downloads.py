75e5da6e8c5f9260a800c99a985ae822c03dbf2b
alexander-g <3867427+alexander-g@users.noreply.github.com>
2026-08-20 16:01:17 +0000

    dont try to download a model if the specified file does not exist locally

--- ultralytics/utils/downloads.py
+++ ultralytics/utils/downloads.py
@@ -494,33 +494,36 @@ def attempt_download_asset(
     file = str(file)
     file = checks.check_yolov5u_filename(file)
     file = Path(file.strip().replace("'", ""))
-    if file.exists():
-        return str(file)
-    elif (SETTINGS["weights_dir"] / file).exists():
-        return str(SETTINGS["weights_dir"] / file)
-    else:
-        # URL specified
-        name = Path(parse.unquote(str(file))).name  # decode '%2F' to '/' etc.
-        download_url = f"https://github.com/{repo}/releases/download"
-        if str(file).startswith(("http:/", "https:/")):  # download
-            url = str(file).replace(":/", "://")  # Pathlib turns :// -> :/
-            file = url2file(name)  # parse authentication query strings
-            if Path(file).is_file():
-                LOGGER.info(f"Found {clean_url(url)} locally at {file}")  # file already exists
-            else:
-                safe_download(url=url, file=file, min_bytes=1e5, **kwargs)
+    assert file.exists(), file
+    return str(file)
+    
+    # if file.exists():
+    #     return str(file)
+    # elif (SETTINGS["weights_dir"] / file).exists():
+    #     return str(SETTINGS["weights_dir"] / file)
+    # else:
+    #     # URL specified
+    #     name = Path(parse.unquote(str(file))).name  # decode '%2F' to '/' etc.
+    #     download_url = f"https://github.com/{repo}/releases/download"
+    #     if str(file).startswith(("http:/", "https:/")):  # download
+    #         url = str(file).replace(":/", "://")  # Pathlib turns :// -> :/
+    #         file = url2file(name)  # parse authentication query strings
+    #         if Path(file).is_file():
+    #             LOGGER.info(f"Found {clean_url(url)} locally at {file}")  # file already exists
+    #         else:
+    #             safe_download(url=url, file=file, min_bytes=1e5, **kwargs)
 
-        elif repo == GITHUB_ASSETS_REPO and name in GITHUB_ASSETS_NAMES:
-            safe_download(url=f"{download_url}/{release}/{name}", file=file, min_bytes=1e5, **kwargs)
+    #     elif repo == GITHUB_ASSETS_REPO and name in GITHUB_ASSETS_NAMES:
+    #         safe_download(url=f"{download_url}/{release}/{name}", file=file, min_bytes=1e5, **kwargs)
 
-        else:
-            tag, assets = get_github_assets(repo, release)
-            if not assets:
-                tag, assets = get_github_assets(repo)  # latest release
-            if name in assets:
-                safe_download(url=f"{download_url}/{tag}/{name}", file=file, min_bytes=1e5, **kwargs)
+    #     else:
+    #         tag, assets = get_github_assets(repo, release)
+    #         if not assets:
+    #             tag, assets = get_github_assets(repo)  # latest release
+    #         if name in assets:
+    #             safe_download(url=f"{download_url}/{tag}/{name}", file=file, min_bytes=1e5, **kwargs)
 
-        return str(file)
+    #     return str(file)
 
 
 def download(
